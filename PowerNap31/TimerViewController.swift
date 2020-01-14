c0a8b01dc9bf9d80669f4574683c7a54a30035ab
jon <jdcorn@live.com>
2020-01-14 11:20:16 -0700

    added timer protocol/delegate

--- PowerNap31/TimerViewController.swift
+++ PowerNap31/TimerViewController.swift
@@ -20,14 +20,47 @@ class TimerViewController: UIViewController {
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+        myTimer.delegate = self
     }
     
     // MARK: - Actions
     @IBAction func startButtonTapped(_ sender: Any) {
-        
+        if myTimer.isOn {
+            myTimer.stopTimer()
+        } else {
+        myTimer.startTimer(10)
+        }
+        updateViews()
     }
+    
+    // MARK: - Functions
+    func updateViews() {
+        updateTimerTextLabel()
+        if myTimer.isOn {
+            startButton.setTitle("Stop", for: .normal)
+        } else {
+            startButton.setTitle("Start nap", for: .normal)
+        }
+    }
+    
+    func updateTimerTextLabel() {
+        timerLabel.text = myTimer.timeRemainingAsString()
+    }
+    
 }
 
 extension TimerViewController: TimerDelegate {
+    func timerSecondTick() {
+        updateTimerTextLabel()
+    }
+    
+    func timerCompleted() {
+        updateViews()
+    }
+    
+    func timerStopped() {
+        updateViews()
+    }
     
 }
